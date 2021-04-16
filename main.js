777a661df08f7f101d1cf2ca80f118e48bfb0e4c
chiefanunzio <chiefa.nunzio@gmail.com>
2021-04-16 18:52:59 +0200

    .sort ok

--- main.js
+++ main.js
@@ -3,49 +3,69 @@ function initVue() {
     new Vue({
         el: '#app',
         data: {
-  
-          values:[],
-          filter:[],
-          filtered:[],
+
+            values: [],
+            genere: '',
+
         },
-        mounted(){
-  
-          axios.get('https://flynn.boolean.careers/exercises/api/array/music')
-    
-          .then(data => {
-              
-              this.values = data.data.response;
-              
-              for (let i = 0; i < this.values.length; i++) {
-                const elem = this.values[i].genre;
-                if(!this.filter.includes(elem)){
-  
-                  this.filter.push(elem);
+        mounted() {
+
+            axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+
+            .then(data => {
+                
+                this.values = data.data.response;
+                
+                
+                
+            })
+            
+        },
+        computed: {
+            
+            generi: function () {
+                const filter = [];
+                
+                for (let i = 0; i < this.values.length; i++) {
+                    const elem = this.values[i].genre;
+                    const years = this.values[i].year;
+                    if (!filter.includes(elem)) {
+                        
+                        filter.push(elem);
+                    }
+                
+                    
+                }
+                return filter;
+            },
+            
+            filteredAlbum: function(){
+                
+                if(this.genere != ''){
+                    //old arr        iesimo    genere     valore v-model     
+                    return this.values.filter(album => album.genre == this.genere).sort(function (a,b){
+                        return (a.year - b.year);
+                    });//ritorna un arrey nuovo di pacca
+                }else {
+                    return this.values.sort(function (a,b){
+                        return (a.year - b.year);
+                    }); // old arr
                 }
                 
-              }
-              console.log(this.filter);
-              console.log(this.filtered);
-              
-              
-              
-            })
-  
-        },
+            },
+            
+        }
         
         
         
     })
-  }
-  
-  function init() {
-  
-    // const autoBtn = $('#auto-btn')
-    // const btn = $('#btn');
-    // btn.click(boxGenetaror);
-    // autoBtn.click(generateAuto);
-    initVue()
-    console.log()
+}
+
+function init() {
     
+    
+    initVue()
+    
+   
 }
 document.addEventListener('DOMContentLoaded', init);
